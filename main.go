5be975bb2dd1dbf8ab4da8c2c36c2710a55afe39
David Agrest <david.agrest@snyk.io>
2022-06-13 12:55:55 +0300

    add circleci configuartion yaml

--- main.go
+++ main.go
@@ -3,9 +3,41 @@ package main
 import (
 	"fmt"
 	logic "gocircleci/app/src"
+	"net/http"
+	"sync"
 )
 
+var one sync.Once
+
+var instance *http.Client
+
+// State Enum in GO
+type State string
+
+// State Enum in GO
+const (
+	CANCELLED = "CANCELLED"
+	OK        = "OK"
+	ERROR     = "ERROR"
+)
+
+func GetHttpClient() *http.Client {
+
+	one.Do(func() {
+		var client http.Client
+		instance = &client
+	})
+	return instance
+}
+
 func main() {
+
+	var myState State
+	myState = CANCELLED
+	fmt.Println(myState)
+	myState = OK
+	fmt.Println(myState)
+
 	fmt.Println("Hello CircleIC")
 	fmt.Printf("Foo parameter is: %d", 100)
 	logic.WhichInput(100)
